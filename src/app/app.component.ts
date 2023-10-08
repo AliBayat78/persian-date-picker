ec50bcee724205ca5f9421945344bd99d8700afb
bayat <m.bayat@sanadpardaz.com>
2023-10-08 17:37:46 +0330

    internationalization resolved styles and directions have bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ServiceService } from './service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  minDate1 = '1200/01/10'
-  maxDate1 = '1600/01/01'
-  constructor(private fb: FormBuilder) {
+  minDate1 = '1300/01/10'
+  maxDate1 = '1500/01/01'
+  constructor(private fb: FormBuilder, private languageService: ServiceService) {
     this.formGroup = fb.group({
       customDate1: ['', [Validators.required]],
     });
@@ -33,5 +34,14 @@ export class AppComponent {
       alert('error');
       console.log(this.customDate1);
     }
+  }
+
+  setLang() {
+   if(this.languageService.getLanguage() === 'fa') {
+    this.languageService.setLanguage('en')
+   } else {
+    this.languageService.setLanguage('fa')
+   }
+   window.location.reload()
   }
 }
